3a5913f2d0e0ef79c2fb69f8fb95b16a11925ed5
Prashanta <prashanta.bct73054@kecktm.edu.np>
2022-12-05 17:28:05 +0545

    Functional Play Button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
 // Initialized Variables
 
 let songIndex = 0;
-let audioElement = new Audio("assets/music/Cant Take My Eyes off You.mp3");
+
 const masterPlay = document.querySelector("#masterPlay");
 const progressBar = document.querySelector("#progressBar");
 const gif = document.querySelector(".gif");
-const songItems = Array.from(document.querySelector(".songitem"));
+const songItems = Array.from(document.querySelectorAll(".songitem"));
+const currentlyPlayingTitle = document.querySelector(".currentlyplayingtitle");
 
 let songs = [
   {
@@ -20,7 +21,7 @@ let songs = [
   },
   {
     songName: "Coolio - Gangsta's Paradise ",
-    filePath: "assets/music/Coolio - Gangsta's Paradise (Lyrics) ft. L.V..mp3",
+    filePath: "assets/music/Coolio - Gangsta's Paradise (Lyrics) ft. L.V..m4a",
     coverPath: "assets/img/cover1.jpg",
   },
   {
@@ -45,7 +46,7 @@ let songs = [
     coverPath: "assets/img/cover1.jpg",
   },
   {
-    songName: "Sasha Alex Sloan - Dancing With Your Ghost",
+    songName: "Sasha - Dancing With Your Ghost",
     filePath:
       "assets/music/Sasha Alex Sloan - Dancing With Your Ghost (Lyric Video).mp3",
     coverPath: "assets/img/cover1.jpg",
@@ -63,26 +64,54 @@ let songs = [
   },
 ];
 
+let audioElement = new Audio(
+  "assets/music/Ghost - Mary On A Cross (Official Audio).mp3"
+);
+
+// Add songs
+const songList = document.querySelector(".songitemcontainer");
+songs.forEach((song) => {
+  songList.innerHTML += `
+     <div class="songitem">
+     <img src=${song.coverPath} alt="Cover">
+     <h3>${song.songName}</h3>
+     <span class="songlistplay" >
+     <h3 class="timestamp">
+     04:05
+     <i class="far fa-circle-play songitemplay"></i>
+     </h3>
+     </span>
+  </div>
+    `;
+});
+
 // Getting song items
 
 songItems.forEach((element, i) => {
-  console.log(element, i);
-  console.log("Hello");
-  element.getElementsByTagName("img")[0].src = songs[i].filePath;
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByTagName("h3")[0].innerText = songs[i].songName;
 });
 
 // Events
+const playFn = (main) => {
+  main.classList.remove("fa-play-circle");
+  main.classList.add("fa-pause-circle");
+  gif.style.opacity = 1;
+};
+
+const pauseFn = (main) => {
+  main.classList.remove("fa-pause-circle");
+  main.classList.add("fa-play-circle");
+  gif.style.opacity = 0;
+};
+
 masterPlay.addEventListener("click", () => {
   if (audioElement.paused || audioElement.currentTime <= 0) {
     audioElement.play();
-    masterPlay.classList.remove("fa-play-circle");
-    masterPlay.classList.add("fa-pause-circle");
-    gif.style.opacity = 1;
+    playFn(masterPlay);
   } else {
     audioElement.pause();
-    masterPlay.classList.remove("fa-pause-circle");
-    masterPlay.classList.add("fa-play-circle");
-    gif.style.opacity = 0;
+    pauseFn(masterPlay);
   }
 });
 
@@ -96,4 +125,27 @@ audioElement.addEventListener("timeupdate", () => {
 
 progressBar.addEventListener("change", () => {
   audioElement.currentTime = (progressBar.value * audioElement.duration) / 100;
+});
+
+const makePlays = () => {
+  Array.from(document.querySelectorAll(".songitemplay")).forEach((element) => {
+    pauseFn(element);
+  });
+};
+
+Array.from(document.querySelectorAll(".songitemplay")).forEach((element) => {
+  element.addEventListener("click", (e) => {
+    makePlays();
+    playFn(e.target);
+    audioElement.currentTime = 0;
+    audioElement.play();
+    playFn(masterPlay);
+    masterPlay.addEventListener("click", () => {
+      if (audioElement.paused) {
+        pauseFn(e.target);
+      } else {
+        playFn(e.target);
+      }
+    });
+  });
 });
